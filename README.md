1f9972ebe2d0c282b23f61fd9452ea0536aefaee
AwaisIsane <awaisane@gmail.com>
2021-02-11 00:17:22 +0530

    cc

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-THE link to hero ku app is https://pacific-everglades-02712.herokuapp.com/
+THE link to heroku app is https://pacific-everglades-02712.herokuapp.com/
--- index.js
+++ index.js
@@ -1,10 +1,12 @@
+require('dotenv').config()
 const { response } = require('express');
 const express = require('express');
 const cors = require('cors')
-
+const mongoose = require('mongoose')
 const app = express();
-
 const morgan = require('morgan');
+const Person = require("./models/person")
+
 
 morgan.token('content', function (req, res) { 
   return JSON.stringify(req.body) })
@@ -21,46 +23,29 @@ app.use(logger);
 
 app.use(express.json());
 
-let persons = [
-    {
-        "name": "Arto Hellas",
-        "number": "040-123456",
-        "id": 1
-      },
-      {
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523",
-        "id": 2
-      },
-      {
-        "name": "Dan Abramov",
-        "number": "12-43-234345",
-        "id": 3
-      },
-      {
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122",
-        "id": 4
-      }
-    
-]
 
 app.get("/api/persons",(req,res)  => {
-    res.json(persons)
+    Person.find({}).then(result => {
+    console.log("phonebook:")
+    result.forEach(note => {
+    console.log(`${note.name} ${note.number}`)
+    })
+    res.json(result)
+
+
+  })
 })
 
 app.get("/api/persons/:id", (req,res) => {
-  const id = Number(req.params.id);
-  const person =  persons.find(per =>per.id===id)
-
-  if (person) {
-    res.json(person)
-    }
-    else {
-      res.status(404).end()
-    }
+  Person.findById(req.params.id)
+  .then(per => {
+    res.json(per)
+  }
+  )
+  .catch(err => {
+    res.status(404).end()
+    console.log("err",err)})
 })
-
 
 app.get("/info",(req,res) => {
     const strtosen = `<p>Phonebook has info of ${persons.length} people
@@ -70,8 +55,7 @@ app.get("/info",(req,res) => {
 )
 
 app.delete("/api/persons/:id", (req,res) => {
-  const id = Number(req.params.id)
-  persons = persons.filter(per => per.id !== id)
+  Person.deleteOne({id:req.params.id})
   res.status(204).end()
 })
 
@@ -86,23 +70,33 @@ app.post("/api/persons" , (req,res) => {
       error: 'content missing' 
     })
   }
-
+/*
   if (persons.filter(per => per.name===body.name).length!==0) {
     return res.status(400).json({ 
       error: 'name must be unique' 
     })
   }
-
-  const per = {
+*/
+  const person = new Person({
     name:body.name,
     number:body.number,
-    id:genrateId(),
-  }
-  persons = persons.concat(per)
-  res.json(per)
+  })
+
+  person.save().then(per => {
+    console.log(`added ${per.name} number ${per.number} to phonebook`)
+    res.json(per)
+  })
+
 })
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
+
+process.on('SIGINT', function() {
+  mongoose.connection.close(function () {
+    console.log('Mongoose disconnected on app termination');
+    process.exit(0);
+  });
+});
